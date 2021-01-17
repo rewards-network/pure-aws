0d534996fbf458016f003252303062d795a53c08
Ryan Peters <me@rpeters.dev>
2021-01-17 16:03:14 -0600

    Fix TestS3Sink and expand it to allow failing

--- modules/s3-testing/src/main/scala/com/rewardsnetwork/pureaws/s3/testing/TestS3Sink.scala
+++ modules/s3-testing/src/main/scala/com/rewardsnetwork/pureaws/s3/testing/TestS3Sink.scala
@@ -1,15 +1,33 @@
 package com.rewardsnetwork.pureaws.s3.testing
 
 import cats.effect.Sync
+import cats.implicits._
 import com.rewardsnetwork.pureaws.s3.S3Sink
+import fs2.{Pipe, Stream}
 
-import fs2.Pipe
+class TestS3Sink[F[_]: Sync](backend: S3TestingBackend[F], failUploadWith: Option[Throwable] = none) extends S3Sink[F] {
 
-class TestS3Sink[F[_]: Sync](backend: S3TestingBackend[F]) extends S3Sink[F] {
+  private def doOrFail[A](pipe: Pipe[F, Byte, Unit]): Pipe[F, Byte, Unit] = failUploadWith match {
+    case Some(t) => _ >> Stream.raiseError[F](t)
+    case None    => pipe
+  }
+
   def writeText(bucket: String, key: String): Pipe[F, Byte, Unit] = { s =>
     s.chunkAll.map(_.toByteBuffer.array).evalMap(backend.put(bucket, key, _))
   }
 
-  def writeBytes(bucket: String, key: String, contentType: String): fs2.Pipe[F, Byte, Unit] = writeText(bucket, key)
+  def writeTextMultipart(bucket: String, key: String, partSizeBytes: Int): fs2.Pipe[F, Byte, Unit] =
+    writeText(bucket, key)
+
+  def writeBytes(bucket: String, key: String, contentType: String): fs2.Pipe[F, Byte, Unit] = doOrFail {
+    writeText(bucket, key)
+  }
+
+  def writeBytesMultipart(
+      bucket: String,
+      key: String,
+      contentType: String,
+      partSizeBytes: Int
+  ): fs2.Pipe[F, Byte, Unit] = writeBytes(bucket, key, contentType)
 
 }
