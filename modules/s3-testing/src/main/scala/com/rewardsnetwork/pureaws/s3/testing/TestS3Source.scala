4d697453a0795bfad545a2fd00aeed70490e33e2
Ryan Peters <me@rpeters.dev>
2021-01-17 14:51:23 -0600

    Use Fs2AsyncResponseTransformer for async streams

--- modules/s3-testing/src/main/scala/com/rewardsnetwork/pureaws/s3/testing/TestS3Source.scala
+++ modules/s3-testing/src/main/scala/com/rewardsnetwork/pureaws/s3/testing/TestS3Source.scala
@@ -9,6 +9,10 @@ import fs2.Stream
 class TestS3Source[F[_]: Sync](backend: S3TestingBackend[F]) extends S3Source[F] {
   def readObject(bucket: String, key: String): Stream[F, Byte] =
     Stream.eval(readObjectWithMetadata(bucket, key)).flatMap(_._2)
+
+  def readWholeObject(bucket: String, key: String)(implicit F: Applicative[F]): F[Array[Byte]] =
+    readObject(bucket, key).compile.to(Array)
+
   def readObjectWithMetadata(bucket: String, key: String)(implicit
       F: Applicative[F]
   ): F[(Map[String, String], Stream[F, Byte])] =
@@ -16,4 +20,9 @@ class TestS3Source[F[_]: Sync](backend: S3TestingBackend[F]) extends S3Source[F]
       case Some((meta, payload)) => (meta -> Stream.emits(payload.toList).covary[F]).pure[F]
       case None                  => Sync[F].raiseError(new Exception("Object not found"))
     }
+
+  def readWholeObjectWithMetadata(bucket: String, key: String)(implicit
+      F: Applicative[F]
+  ): F[(Map[String, String], Array[Byte])] =
+    readObjectWithMetadata(bucket, key).flatMap { case (meta, stream) => stream.compile.to(Array).tupleLeft(meta) }
 }
