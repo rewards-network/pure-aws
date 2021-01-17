e918a87d6f25a45d181af9d5d54ec26360b878a8
Ryan Peters <me@rpeters.dev>
2021-01-17 16:15:08 -0600

    Scalacheck-ify Fs2AsyncResponseTransformerSpec

--- modules/core/src/test/scala/com/rewardsnetwork/pureaws/Fs2AsyncResponseTransformerSpec.scala
+++ modules/core/src/test/scala/com/rewardsnetwork/pureaws/Fs2AsyncResponseTransformerSpec.scala
@@ -8,29 +8,31 @@ import software.amazon.awssdk.core.internal.async.SdkPublishers
 
 import scala.concurrent.ExecutionContext.global
 import java.nio.ByteBuffer
+import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
 
-class Fs2AsyncResponseTransformerSpec extends AnyFreeSpec with Matchers {
+class Fs2AsyncResponseTransformerSpec extends AnyFreeSpec with Matchers with ScalaCheckPropertyChecks {
   implicit val cs = IO.contextShift(global)
   implicit val timer = IO.timer(global)
 
   "Fs2AsyncResponseTransformer" - {
     "should provide the stream of bytes to the user" in {
-      val exampleStr = "Hello world!"
-      val exampleStrBytes = exampleStr.getBytes
-      val exampleStrByteBuf = ByteBuffer.wrap(exampleStrBytes)
-      val transformer = Fs2AsyncResponseTransformer[IO, String]
-      val fs2publisher = fs2.Stream.emit[IO, ByteBuffer](exampleStrByteBuf).toUnicastPublisher
-      val sdkPublisher = SdkPublishers.envelopeWrappedPublisher(fs2publisher, "", "")
-      val cf = transformer.prepare()
+      forAll { exampleStr: String =>
+        val exampleStrBytes = exampleStr.getBytes
+        val exampleStrByteBuf = ByteBuffer.wrap(exampleStrBytes)
+        val transformer = Fs2AsyncResponseTransformer[IO, String]
+        val fs2publisher = fs2.Stream.emit[IO, ByteBuffer](exampleStrByteBuf).toUnicastPublisher
+        val sdkPublisher = SdkPublishers.envelopeWrappedPublisher(fs2publisher, "", "")
+        val cf = transformer.prepare()
 
-      //Use the transformer the way the SDK would
-      transformer.onResponse(exampleStr)
-      transformer.onStream(sdkPublisher)
+        //Use the transformer the way the SDK would
+        transformer.onResponse(exampleStr)
+        transformer.onStream(sdkPublisher)
 
-      //Get and test results
-      val (str, byteStream) = cf.get()
-      str shouldBe exampleStr
-      byteStream.compile.to(Array).unsafeRunSync() shouldBe exampleStrBytes
+        //Get and test results
+        val (str, byteStream) = cf.get()
+        str shouldBe exampleStr
+        byteStream.compile.to(Array).unsafeRunSync() shouldBe exampleStrBytes
+      }
     }
   }
 }
