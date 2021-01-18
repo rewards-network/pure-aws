1a44ba8b1a44af54e9fda9585cd1bb1058ed373e
Ryan Peters <me@rpeters.dev>
2021-01-17 22:19:06 -0600

    Undo throwing errors in s3 test helpers (for future PR)

--- modules/s3-testing/src/main/scala/com/rewardsnetwork/pureaws/s3/testing/TestS3Sink.scala
+++ modules/s3-testing/src/main/scala/com/rewardsnetwork/pureaws/s3/testing/TestS3Sink.scala
@@ -1,26 +1,18 @@
 package com.rewardsnetwork.pureaws.s3.testing
 
 import cats.effect.Sync
-import cats.implicits._
 import com.rewardsnetwork.pureaws.s3.S3Sink
-import fs2.{Pipe, Stream}
+import fs2.Pipe
 
 /** A test utility for integrating with the `S3Sink` algebra.
   *
   * @param backend Your `S3TestingBackend`.
-  * @param failUploadWith A `Throwable` that you would like all requests to this sink to fail with, to test error recovery.
   * @param emitEtag An iterator of strings for sequentially assigning eTags.
   */
 class TestS3Sink[F[_]: Sync](
     backend: S3TestingBackend[F],
-    failUploadWith: Option[Throwable] = none,
     emitEtag: Iterator[String] = Iterator.from(0).map(_.toString)
 ) extends S3Sink[F] {
-
-  private def doOrFail[A](pipe: Pipe[F, Byte, String]): Pipe[F, Byte, String] = failUploadWith match {
-    case Some(t) => _ >> Stream.raiseError[F](t)
-    case None    => pipe
-  }
 
   def writeText(bucket: String, key: String): Pipe[F, Byte, String] = { s =>
     s.chunkAll.map(_.toByteBuffer.array).evalMap(backend.put(bucket, key, _)).as(emitEtag.next())
@@ -29,9 +21,8 @@ class TestS3Sink[F[_]: Sync](
   def writeTextMultipart(bucket: String, key: String, partSizeBytes: Int): fs2.Pipe[F, Byte, String] =
     writeText(bucket, key)
 
-  def writeBytes(bucket: String, key: String, contentType: String): fs2.Pipe[F, Byte, String] = doOrFail {
+  def writeBytes(bucket: String, key: String, contentType: String): fs2.Pipe[F, Byte, String] =
     writeText(bucket, key)
-  }
 
   def writeBytesMultipart(
       bucket: String,
